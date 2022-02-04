6fca0248e4504740baf6acf3d8f8d56b6100f3e6
Leonardo Dreher <leonardodreher@gmail.com>
2022-02-03 22:52:06 -0300

    edições do dia 03 da aula 6 a 10

---
+++ exercicios6.10/estrutura_de_controle09.php
@@ -0,0 +1,63 @@
+<?php
+print 'todo o conteúdo está comentado';
+/*
+$salario = 1000;
+$tempo = 12;
+
+if( ($salario < 1000) AND ($tempo >= 12) ){
+    print 'promoção';
+}else{
+    print 'sem promoção';
+}  */
+
+/* Teste 1
+$valor_venda = 100;
+
+if($valor_venda > 100){
+    $resultado = 'muito caro';
+}else{
+    $resultado = 'pode comprar';
+}
+var_dump($resultado);  */
+
+// teste dois sem if 
+/*
+$valor_venda = 100;
+
+$resultado = ($valor_venda > 100)? 'muito caro' : 'pode comprar';
+var_dump($resultado); */
+
+/*Laço de repetição com while */
+/*
+$cont = 1;
+while ($cont <= 5){
+    print $cont . ' ';
+    $cont ++;
+}  */
+
+/*Estrutura de controle com for 
+for ($cont = 1; $cont <= 10; $cont++){ 
+    print $cont . ' ';
+}  */
+
+/*Estrutura de controle com switch que pode subistituir um if */
+/*
+$tipo = 'pdf';
+switch ($tipo){
+    case 'pdf':
+        print 'arquivo PDF';
+    break;
+    case 'doc':
+        print 'arquivo DOC';
+    break;
+    default:
+        print 'arquivo default'; 
+}  */
+
+/*Estrutura de controle que percorre vetor, objetos e variaveis com foreach */
+/*
+$lista = ['maçã', 'laranja', 'banana'];
+
+foreach ($lista as $fruta){
+    print $fruta . ' ';
+} */
